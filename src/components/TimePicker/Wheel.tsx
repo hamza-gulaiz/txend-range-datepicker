3896128cfd82d2e5f712a49a33a63db5aa399a69
Hamza <hamza.gulraiz@txend.com>
2024-01-18 20:48:40 +0500

    wheel code deleted

--- src/components/TimePicker/Wheel.tsx
+++ src/components/TimePicker/Wheel.tsx
@@ -1,66 +1,11 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker'
-
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 const TimerPicker = () => {
-  const [hours, setHours] = useState('0');
-  const [minutes, setMinutes] = useState('0');
-  // const [seconds, setSeconds] = useState('0');
 
-  const handleHoursChange = (value: React.SetStateAction<string>) => {
-    setHours(value);
-  };
-
-  const handleMinutesChange = (value: React.SetStateAction<string>) => {
-    setMinutes(value);
-  };
-
-  // const handleSecondsChange = (value: React.SetStateAction<string>) => {
-  //   setSeconds(value);
-  // };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={hours}
-          onValueChange={handleHoursChange}
-          style={styles.picker}
-        >
-          {[...Array(12).keys()].map((hour) => (
-            <Picker.Item key={hour} label={`${hour}`} value={`${hour}`} />
-          ))}
-        </Picker>
-        <Text style={styles.label}>Hours</Text>
-      </View>
-
-      <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={minutes}
-          onValueChange={handleMinutesChange}
-          style={styles.picker}
-        >
-          {[...Array(60).keys()].map((minute) => (
-            <Picker.Item key={minute} label={`${minute}`} value={`${minute}`} />
-          ))}
-        </Picker>
-        <Text style={styles.label}>Minutes</Text>
-      </View>
-
-      {/* <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={seconds}
-          onValueChange={handleSecondsChange}
-          style={styles.picker}
-        >
-          {[...Array(60).keys()].map((second) => (
-            <Picker.Item key={second} label={`${second}`} value={`${second}`} />
-          ))}
-        </Picker>
-        <Text style={styles.label}>Seconds</Text>
-      </View> */}
-    </View>
+    <View style={styles.container} />
   );
 };
 
